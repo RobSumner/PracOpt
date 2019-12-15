a4e85363bf7e6ed18f024eed9f5493c87ee3335e
Rob Sumner <rob_sumner1@hotmail.co.uk>
2019-12-15 17:41:19 +0000

    Added initial temperature calculation plus tests.

--- optimiser/optimiser.py
+++ optimiser/optimiser.py
@@ -3,6 +3,7 @@
 Classes
 -------
 TrialMode - Enumerated class to describe trial update mode. 
+InitialTempMode - Enumerated class to describe initial temperature mode.
 SimAnneal - Class to perform Simulated Annealing optimisation
 
 """
@@ -17,6 +18,12 @@ class TrialMode(Enum):
    CHOLESKY = 2
    PARKS = 3
 
+class InitialTempMode(Enum):
+   """Mode describing the initial temperature calculation."""
+   PRESET = 1
+   KIRKPATRICK = 2
+   WHITE = 3
+
 class SimAnneal:
    """Perform Simulated Annealing Optimisiation
    Parameters
@@ -25,35 +32,80 @@ class SimAnneal:
 
    Public Methods
    -------------- 
+   new_trial_solution - Return a new trial solution based upon trial mode. 
+   uniform_random - Return 1D sample from uniform variable in range
+                    x_min, x_max. 
+   initial_temp - Set the initial temperature. 
    """
 
-   def __init__(self, objective, trial_mode):
+   def __init__(self, objective, trial_mode, initial_temp_mode):
       """Initialise the optimiser."""
-      self.objective = objective
-      self.dimension = objective.n
-      self.iteration = 0
-
-      # Point, Objective value & change in objective.
-      self.x = np.zeros((self.dimension, 1))
-      self.current_f = self.objective.f(self.x)
-      self.df = 0
-      
+      # Check input arguments
       if type(trial_mode) != TrialMode:
          raise ValueError('Trial Mode not recognised.')
       self.trial_mode = trial_mode
+      if type(initial_temp_mode) != InitialTempMode:
+         raise ValueError('Initial Temperature Mode not recognised.')
+      self.initial_temp_mode = initial_temp_mode
 
-   def new_trial(self):
-      """Return new trial solution following trial mode."""
+      # Set objective function
+      self.objective = objective
+      self.dimension = objective.n
+
+      # Point, Objective value & change in objective.
+      self.x = np.zeros((self.dimension, 1)) # Sample point
+
+      # Annealing schedule
+      self.current_T = 10000
+      self.max_evaluations = 10000 
+
+   def run(self):
+      """Perform the optimisation."""
+      while self.objective.evaulations < self.max_evaluations:
+         # Find a new valid solution
+         f0 = self.objective.f(self.x)
+         x1 = self.new_trial_solution()
+         while not self.acceptable_solution(self.objective.f(x1) - f0):
+            x1 = self.new_trial_solution()
+         self.x = x1  
+
+         # Update temperature. 
+      return self.x     
+      
+   def acceptable_solution(self, df):
+      """Return True if solution decreases objective function, or based
+         on acceptance probability if f increases. Else, return False."""
+      if df < 0:
+         # Always accept if f decreases.
+         return True
+      else:
+         p_accept = np.exp(-1*df/self.current_T)
+         sample = self.uniform_random(0,1)
+         if sample <= p_accept:
+            return True
+         else:
+            return False
+
+   def new_trial_solution(self, x0=None):
+      """Return new trial solution following trial mode.
+         If given, x0 is used as starting point for the new trial x.
+         Otherwise, current self.x value is used."""      
+      # Set start point if none provided.
+      if x0 is None:
+         x0 = self.x
+      elif len(x0) != self.dimension:
+         raise ValueError('Incorrect starting point dimension for new trial.')
+
       # Simple diagonal (C) matrix update.
       if self.trial_mode is TrialMode.BASIC:
-         x_new = self.x
+         x_new = x0
          for i in range(self.dimension):
             # Sample new position until it is feasible
             u_i = self.uniform_random(self.objective.x_min, self.objective.x_max)
-            x_new[i] = self.x[i] + u_i
+            x_new[i] = x0[i] + u_i
             while not self.objective.is_feasible(x_new, i):
                u_i = self.uniform_random(self.objective.x_min, self.objective.x_max)
-               x_new[i] = self.x[i] + u_i
+               x_new[i] = x0[i] + u_i
          return x_new
       else:
          return np.zeros((self.dimension, 1))
@@ -65,6 +117,47 @@ class SimAnneal:
 
       x_range = x_max - x_min
       return (np.random.rand()*x_range) + x_min
+
+   def initial_temp(self):
+      """Set the initial temperature.
+         Kirkpatrick [1984] - Set T0 so av. prob. of solution increasing f
+         is ~0.8.
+         White [1984] - Use sigma (sd of variation in objective function) s.t.
+         T0 = sigma.
+      """
+      if self.initial_temp_mode is InitialTempMode.PRESET:
+         # Temp already set in class - no calculation needed.
+         return
+      
+      # Calculate T0 following White/Kirkpatrick
+      samples = 100
+      x_range = self.objective.x_max - self.objective.x_min
+      df_samples = np.zeros((samples,1))
+      for i in range(samples):
+         # Random starting point in range [-2,2]
+         x0 = np.random.rand(self.dimension, 1)*x_range + self.objective.x_min
+         f0 = self.objective.f(x0)
          
+         # Accept any move which increase value of f.  
+         x1 = self.new_trial_solution(x0)
+         f1 = self.objective.f(x1)
+         # Limit to 10 trials for speed.
+         for _ in range(10):
+            if f1 <= f0:
+               x1 = self.new_trial_solution(x0)
+               f1 = self.objective.f(x1)
+            else:
+               df_samples[i] = f1 - f0
+      # Extract zeros from array (failed trials)
+      df_samples = df_samples[df_samples != 0]
+
+      # Set T0 based on mode. 
+      if self.initial_temp_mode is InitialTempMode.KIRKPATRICK:
+         # Set T to be average
+         self.current_T = -1*np.mean(df_samples)/np.log(0.8)
+      elif self.initial_temp_mode is InitialTempMode.WHITE:
+         # Calculate sd of samples
+         self.current_T = np.std(df_samples) 
+ 
 
       
--- tests/test_sim_anneal.py
+++ tests/test_sim_anneal.py
@@ -2,8 +2,8 @@
 
 import pytest
 import numpy as np
-from optimiser.optimiser import SimAnneal, TrialMode
-from optimiser.objective import Shubert
+from optimiser.optimiser import SimAnneal, TrialMode, InitialTempMode
+from optimiser.objective import Shubert, ObjectiveTest
 
 @pytest.fixture
 def new_sim2():
@@ -11,7 +11,7 @@ def new_sim2():
       with 2D Shubert objective function.
    """
    obj = Shubert(2)
-   return SimAnneal(obj, TrialMode.BASIC)
+   return SimAnneal(obj, TrialMode.BASIC, InitialTempMode.KIRKPATRICK)
 
 @pytest.fixture
 def new_sim5():
@@ -19,20 +19,28 @@ def new_sim5():
       with 2D Shubert objective function.
    """
    obj = Shubert(5)
-   return SimAnneal(obj, TrialMode.BASIC)
+   return SimAnneal(obj, TrialMode.BASIC, InitialTempMode.KIRKPATRICK)
+
+@pytest.fixture
+def new_test_sim():
+   """Return a new instance of the simulated annealing class
+      with 5D test function.
+   """
+   obj = ObjectiveTest()
+   return SimAnneal(obj, TrialMode.BASIC, InitialTempMode.KIRKPATRICK)
 
 def test_sim_anneal_init(new_sim2, new_sim5):
    """Test initialisation of the simulated annealing class."""
    assert type(new_sim2.objective) == Shubert
-   assert new_sim2.iteration == 0
    assert new_sim2.trial_mode == TrialMode.BASIC
 
    assert type(new_sim5.objective) == Shubert
-   assert new_sim5.iteration == 0
    assert new_sim5.trial_mode == TrialMode.BASIC
 
-def test_sim_anneal_rand(new_sim5):
+def test_sim_anneal_rand(new_sim5, new_test_sim):
    """Test random number generator."""
+   # Fix seed for reproducible results
+   np.random.seed(seed=1)
    with pytest.raises(ValueError):
       new_sim5.uniform_random(1,-1)
 
@@ -41,6 +49,50 @@ def test_sim_anneal_rand(new_sim5):
       assert sample >= -1
       assert sample <= 1
 
-def test_sim_anneal_new_trial(new_sim5):
+   samps = 200
+   samples = np.zeros((samps,1))
+   for i in range (samps):
+      samples[i] = new_test_sim.uniform_random(-2,2)
+   assert round(np.mean(samples)*100)/100 == 0.02
+   assert round(np.std(samples)*100)/100 == 1.22
+
+   # Test positive sample mean
+   samples = samples[samples > 0]
+   assert round(np.mean(samples)*100)/100 == 1
+
+def test_sim_anneal_new_trial_solution(new_sim5):
+   """Test new trial proposal method."""
+   # Check class 
    assert all([a == b for a, b in zip(new_sim5.x, np.zeros((5,1)))])
+   # Test starting point using class start point. 
+   new_x = new_sim5.new_trial_solution()
+   assert all([a != b for a, b in zip(new_x, np.zeros((5,1)))])
+
+   # Test using provided start point
+   new_x = new_sim5.new_trial_solution()
+
+def test_initial_temperature(new_test_sim, new_sim2):
+   """Test initial temperature calculation."""
+   np.random.seed(seed=1)
+
+   # Kirkpatrick method
+   assert new_test_sim.current_T == 10000
+   new_test_sim.initial_temp()
+   assert round(new_test_sim.current_T*100)/100 == 2.56
+
+   # White method
+   new_test_sim.initial_temp_mode = InitialTempMode.WHITE
+   new_test_sim.initial_temp()
+   assert round(new_test_sim.current_T*100)/100 == 0.44
    
+   # Kirkpatric method
+   assert new_sim2.current_T == 10000
+   new_sim2.initial_temp()
+   assert round(new_sim2.current_T/10)*10 == 30
+
+   # White method
+   new_sim2.initial_temp_mode = InitialTempMode.WHITE
+   new_sim2.initial_temp()
+   assert round(new_sim2.current_T) == 8
+
+

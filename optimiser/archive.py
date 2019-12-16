7aeba7b091b6dd14ae7785fcbe9e9e9f9f9c4f19
Rob Sumner <rob_sumner1@hotmail.co.uk>
2019-12-16 12:43:26 +0000

    Added dissimilarity archiving and tests.

--- optimiser/archive.py
+++ optimiser/archive.py
@@ -11,6 +11,7 @@ class Archive:
    """Storage method for optimisation. 
    Parameters
    ----------
+   length - Storage length of the archive. Default value is 20.  
 
    Public Methods
    -------------- 
@@ -20,22 +21,96 @@ class Archive:
                Each row of array is (x_point, f). 
    """
 
-   def __init__(self):
+   def __init__(self, length=20):
       """Initialise the optimiser."""
-      self.x_values = []
-      self.obj_values = []
+      self.all_x_values = []
+      self.all_obj_values = []
+      self.L_length = length # Length of L dissimilarity archive.
+      self.L_x_values = [] # L dissimilarity x values
+      self.L_f_values = [] # L dissimilarity f values
+      self.D_min = 0.1  # Minimum dissimilarity for all elements
+      self.D_sim = 0.01 # Minimum dissimilarity to worst element
    
    def add(self, x, f):
-      """Store the values in the archive."""
+      """Store current values in storage of all solutions and employ
+         Best L-dissimilarity archiving scheme."""
       if not isinstance(x, np.ndarray):
          x = np.array([x])
-      self.x_values.append(x)
-      self.obj_values.append(f)
+      self.all_x_values.append(x)
+      self.all_obj_values.append(f)
+
+      if len(self.L_x_values) > 0:
+         # Worst and Best current solution indices
+         i_worst = np.argmax(self.L_f_values)
+         i_best = np.argmin(self.L_f_values)
+         # Most similar points
+         lowest_sim_val, i_closest = self.most_similar(x)
+ 
+      if len(self.L_x_values) == 0:
+         # Always Store first element
+         self.L_x_values.append(x)
+         self.L_f_values.append(f)
+         return
+         
+      elif len(self.L_x_values) < self.L_length and lowest_sim_val > self.D_min:
+         # Store if archive not full and point is sufficiently 
+         # dissimilar to all current entries.
+         self.L_x_values.append(x)
+         self.L_f_values.append(f)
+         return
+      elif len(self.L_x_values) == self.L_length:
+         # Archive if full, dissimilar and better than one - replace worst.
+         if lowest_sim_val > self.D_min and self.L_f_values[i_worst] > f:
+            self.L_x_values[i_worst] = x
+            self.L_f_values[i_worst] = f
+            return
+
+         # Archive if full, not dissimilar but best - replace most similar
+         elif lowest_sim_val < self.D_min and self.L_f_values[i_best] > f:
+            self.L_x_values[i_closest] = x
+            self.L_f_values[i_closest] = f
+            return
+
+         # Archive if full, not dissimilar, better than most similar 
+         # replace most similar.
+         elif lowest_sim_val < self.D_sim and self.L_f_values[i_closest] > f:
+            self.L_x_values[i_closest] = x
+            self.L_f_values[i_closest] = f
+            return
+      return
 
    def results(self):
       """Convert stored data into a usable format."""
-      n = len(self.x_values)
-      d = len(self.x_values[0])
-      x_data = np.reshape(np.array(self.x_values), (n,d))
-      f_data = np.reshape(np.array(self.obj_values), (n,1))
+      n = len(self.all_x_values)
+      d = len(self.all_x_values[0])
+      x_data = np.reshape(np.array(self.all_x_values), (n,d))
+      f_data = np.reshape(np.array(self.all_obj_values), (n,1))
       return np.concatenate((x_data, f_data), axis=1)
+
+   def most_similar(self, point):
+      """Find the point which is most similar in the archive.
+         Returns the similarity value and index of the point in the archive."""
+      if len(self.L_x_values) == 0:
+         return None, None
+
+      min_sim = 10e10
+      index = 0
+      for i, point_2 in enumerate(self.L_x_values):
+         sim = self.similarity(point, point_2)
+         if  sim < min_sim:
+            min_sim = sim
+            index = i
+      return min_sim, index
+
+   def similarity(self, point_1, point_2):
+      """Find the similarity between two points, based upon a l2-norm 
+         similarity measure."""
+      # Convert into numpy arrays if not currently.
+      if not isinstance(point_1, np.ndarray): point_1 = np.array([point_1])
+      if not isinstance(point_2, np.ndarray): point_2 = np.array([point_2])
+
+      # Check sizes match
+      if point_1.shape != point_2.shape:
+         raise ValueError('Size of points being compared for similarity do not match.')
+
+      return np.linalg.norm(point_1 - point_2, 2)
--- tests/test_archive.py
+++ tests/test_archive.py
@@ -6,26 +6,48 @@ from optimiser.archive import Archive
 
 @pytest.fixture
 def new_arch():
-   """Return an archive populated with some results."""
+   """Return an archive populated with constant f results."""
    arch = Archive()
    for i in range(100):
       arch.add(np.array([i]), 100)
    return arch
 
+@pytest.fixture
+def new_arch2():
+   """Return an archive populated with non constant f results."""
+   arch = Archive()
+   for i in range(100):
+      arch.add(np.array([i]), i)
+   return arch
+
+@pytest.fixture
+def new_arch3():
+   """Return an archive with artificially populated L archive."""
+   arch = Archive()
+   arch.L_x_values = [np.array([1,1]), np.array([1,2]), np.array([3,1])]
+   arch.L_f_values = [1, 2, 3]
+   return arch
+
 def test_archive_init():
    """Test archive initialisation"""
    arch = Archive()
-   assert len(arch.obj_values) == 0
-   assert len(arch.x_values) == 0
+   assert len(arch.all_obj_values) == 0
+   assert len(arch.all_x_values) == 0
 
-def test_archive_store(new_arch):
+def test_archive_add(new_arch, new_arch2, new_arch4):
    """Test the storage methods for archive."""
-   assert len(new_arch.obj_values) == 100
-   assert len(new_arch.x_values) == 100
+   assert len(new_arch.all_obj_values) == 100
+   assert len(new_arch.all_x_values) == 100
 
    new_arch.add(-1,-1)
-   assert len(new_arch.obj_values) == 101
-   assert len(new_arch.x_values) == 101
+   assert len(new_arch.all_obj_values) == 101
+   assert len(new_arch.all_x_values) == 101
+
+   # Test the L dissimilarity archive
+   # Basic adding of numbers 1:100 - 1:20 will be stored
+   assert len(new_arch2.L_f_values) == new_arch2.L_length
+   f_vals = np.linspace(0, new_arch2.L_length, new_arch2.L_length-1)
+   assert sum(new_arch2.L_f_values) == sum(f_vals)
 
 def test_archive_results(new_arch):
    """Test the results method for archive."""
@@ -34,3 +56,34 @@ def test_archive_results(new_arch):
    print(results)
    assert shape[0] == 100
    assert shape[1] == 2
+
+
+@pytest.mark.parametrize("point_1, point_2, value", 
+      [([1], [1], 0),
+       ([1, 1], [1, 1], 0),
+       ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], 0), 
+       ([1, 2], [1, 1], 1),
+       ([5, -2], [-6, 4], 12.529964)])
+
+def test_similarity_measure(new_arch, point_1, point_2, value):
+   """Test the similarity measure values."""
+   with pytest.raises(ValueError):
+      new_arch.similarity([1], [1,1])
+   assert new_arch.similarity(point_1, point_2) == pytest.approx(value)
+
+
+@pytest.mark.parametrize("point, sim, index", 
+      [(np.array([1,1]), 0, 0),
+       (np.array([1,2]), 0, 1),
+       (np.array([3,1]), 0, 2),
+       (np.array([2,1]), 1, 0),
+       (np.array([3,3]), 2, 2)
+       ])
+
+def test_most_similar(new_arch3, point, sim, index):
+   """Test function returning most similar point."""
+   sim_val, index_val = new_arch3.most_similar(point)
+   assert sim_val == sim
+   assert index_val == index
+
+   

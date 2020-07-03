5d9a2b72655bfaf5d48093c14f4b94437924af03
Rob Sumner <rob_sumner1@hotmail.co.uk>
2020-07-03 14:25:26 +0100

    Updated formatting, docstrings in utils.

--- pracopt/utils.py
+++ pracopt/utils.py
@@ -4,55 +4,80 @@ import numpy as np
 import scipy.io
 
 def progress_bar(value, max_value, width=15):
-   """Print a progress bar utilising the carriage return function.
-      value - Number representing the current progress of process.
-      Parameters:
-      max_value - Maximum possible value in process 
-      width - Number of characters in the progress bar.
-   """
-   progress = round(value/max_value*width)
-   remaining = width - progress
-   print('\rOptimisation Progress: ' + "+"*progress + "-"*remaining, end="")
+    """Print progress bar.
 
-def evaluate(Algorithm, runs=25, filepath=None, description=None):
-   """Evaluate the performance of an optimiser class.
-      Parameters:
-      Algorithm - Algorithm to run. 
-      runs - Number of separate optimise runs to perform. 
-      filepath - File path to save results to (as .mat file).
-      description - String to save with results to describe results. 
-      Returns:
-      performance_data - Averaged data on performance over iteration 
-                         and time.
-   """
+    Print a progress bar (utilising the carriage return function).
 
-   max_evals = Algorithm.max_evaluations
-   f_data = np.zeros((max_evals, runs))
-   time_data = np.zeros((max_evals, runs))
+    Parameters
+    ----------
+    value : :obj:`float` or :obj:`int`
+          Number representing the current progress of process.
 
-   for i in range(runs):
-      # Reset all values
-      Algorithm.reset()
+    max_value : :obj:`float` or :obj:`int`
+        Maximum possible value in process.
 
-      # Run the optimisation
-      print("Analysis run: ", i)
-      Algorithm.run()
+    width : :obj:`int`, optional
+        Number of characters in the progress bar. Default is 15.
 
-      # Get objective data
-      data = Algorithm.archive.objective_data(max_evals)
-      f_data[:,i] = data[:,2]
-      time_data[:,i] = data[:,1]
-   
-   # Average across runs.
-   f_average = np.reshape(np.mean(f_data, axis=1), (max_evals,1))
-   t_average = np.reshape(np.mean(time_data, axis=1), (max_evals,1))
-   iters = np.reshape(np.linspace(1, max_evals, max_evals), (max_evals,1))
+    """
+    progress = round(value/max_value*width)
+    remaining = width - progress
+    print('\rOptimisation Progress: ' + "+"*progress + "-"*remaining, end="")
 
-   # Save file if filename is provided
-   if filepath is not None:
-      data_dict = {'f_average':f_average, 't_average':t_average, 'iters':iters}
-      if description is not None:
-         data_dict['description'] = description
-      scipy.io.savemat(filepath + '.mat', data_dict)
-      
-   return np.concatenate((iters, t_average, f_average), axis=1)
+def evaluate(algorithm, runs=5, filepath=None, description=None):
+    """Evaluate optimiser performance.
+
+    Evaluate the performance of an optimiser class. Because of the random
+    search nature of optimiser classes, sevveral evaulation runs are
+    performed and results are averaged.
+
+    Parameters
+    ----------
+    algorithms : :class:`pracopt.optimiser.Optimiser`
+        The optimiser algorithm class to run.
+
+    runs : :obj:`int`, optional.
+        The number of runs to use when evaluating optimiser.
+
+    filepath : :obj:`str`, Optional
+        File path to save results to (as .mat file). If None, file is not
+        saved.
+
+    description : :obj:`str`, optional.
+        Description string to save with results.
+
+    Returns
+    -------
+    results : :class:`numpy.array`
+        Result array. Each row consists of [optimiser step, average time,
+        average value].
+        I.e. the results are averaged across "runs" by objective function
+        step.
+
+    """
+    max_evals = algorithm._max_evaluations
+    f_data = np.zeros((max_evals, runs))
+    time_data = np.zeros((max_evals, runs))
+
+    for i in range(runs):
+        algorithm.reset()
+
+        print("Analysis run: ", i)
+        algorithm.run()
+
+        # Get objective data
+        data = algorithm.archive.objective_data(max_evals)
+        f_data[:,i] = data[:,2]
+        time_data[:,i] = data[:,1]
+
+    f_average = np.reshape(np.mean(f_data, axis=1), (max_evals,1))
+    t_average = np.reshape(np.mean(time_data, axis=1), (max_evals,1))
+    iters = np.reshape(np.linspace(1, max_evals, max_evals), (max_evals,1))
+
+    if filepath is not None:
+        data_dict = {'f_average':f_average, 't_average':t_average, 'iters':iters}
+        if description is not None:
+            data_dict['description'] = description
+        scipy.io.savemat(filepath + '.mat', data_dict)
+
+    return np.concatenate((iters, t_average, f_average), axis=1)

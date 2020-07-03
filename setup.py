6d2de61198f0e80eb6425e3e344560cd83c4ccec
Rob Sumner <rob_sumner1@hotmail.co.uk>
2020-07-03 10:37:10 +0100

    Updated setup file to add more information.

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
 setup(
-    name='optimiser',
-    version='0.1dev',
-    packages=find_packages()
+	name='pracopt',
+	version='0.0.1dev',
+	authors='Rob Sumner',
+	author_email='rob_sumner1@hotmail.co.uk',
+	packages=find_packages(),
+	license='All Rights Reserved',
+	description=('Practical optimisation tools.'),
+	python_requires='>=3.7',
+	install_requires=[],
+	tests_require=['pytest>=4.0']
 )
